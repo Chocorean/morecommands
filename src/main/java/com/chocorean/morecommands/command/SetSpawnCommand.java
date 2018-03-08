20c6e1ae293918d39fb25b15969cb4e418fcbbc7
chocorean <baptiste.chocot@gmail.com>
2018-03-08 08:36:34 +0100

    added /spaw et /setspawn for op only

---
+++ src/main/java/com/chocorean/morecommands/command/SetSpawnCommand.java
@@ -0,0 +1,33 @@
+package com.chocorean.morecommands.command;
+
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.TextComponentString;
+
+public class SetSpawnCommand extends AbstractCommand {
+    @Override
+    public String getCommandName() {
+        return "setspawn";
+    }
+
+    @Override
+    public String getCommandUsage(ICommandSender sender) {
+        return "/setspawn";
+    }
+
+    @Override
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
+        if (!sender.getEntityWorld().isRemote) {
+            sender.getEntityWorld().setSpawnPoint(sender.getPosition());
+            ((EntityPlayerMP)sender).addChatComponentMessage(new TextComponentString("Spawnpoint has been changed."));
+        }
+    }
+
+    @Override
+    public int getRequiredPermissionLevel()
+    {
+        return 4;
+    }
+}
