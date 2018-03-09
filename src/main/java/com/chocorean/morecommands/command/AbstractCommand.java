2f39000247515a3a2354e0ee37e5854934020e41
chocorean <baptiste.chocot@gmail.com>
2018-03-09 14:44:19 +0100

    mother class for all custom commands

---
+++ src/main/java/com/chocorean/morecommands/command/AbstractCommand.java
@@ -0,0 +1,50 @@
+package com.chocorean.morecommands.command;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.math.BlockPos;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class AbstractCommand extends CommandBase implements ICommand {
+    private final ArrayList aliases;
+
+    public AbstractCommand(){
+        aliases = new ArrayList();
+    }
+
+    @Override
+    public List<String> getCommandAliases() {
+        return aliases;
+    }
+
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
+        return true;
+    }
+
+    @Override
+    public List<String> getTabCompletionOptions(MinecraftServer server, ICommandSender sender, String[] args, @Nullable BlockPos pos) {
+        return new ArrayList<String>();
+    }
+
+    @Override
+    public boolean isUsernameIndex(String[] args, int index) {
+        return true;
+    }
+
+    @Override
+    public int compareTo(ICommand iCommand) {
+        return this.getCommandName().compareTo(iCommand.getCommandName());
+    }
+
+    @Override
+    public int getRequiredPermissionLevel()
+    {
+        return 0;
+    }
+}
