75f296fdae0d609d2a9fb09c3682428918e8935c
chocorean <baptiste.chocot@gmail.com>
2019-11-11 23:10:22 +0100

    Update gitignore

--- src/main/java/com/chocorean/morecommands/exception/InvalidGamemodeException.java
+++
@@ -1,9 +0,0 @@
-package com.chocorean.morecommands.exception;
-
-import com.chocorean.morecommands.MoreCommands;
-
-public class InvalidGamemodeException extends MoreCommandsException {
-    public InvalidGamemodeException(String gamemode, Object... objects) {
-        super(String.format(MoreCommands.getConfig().getMessageConfig().getInvalidGamemodeMessage(), gamemode), objects);
-    }
-}
